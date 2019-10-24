b0ea3b73da58d40394d2e0f8545644c82d9a304b
Chad Lengefeld <chadlengefeld@gmail.com>
2019-10-24 17:13:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -32,7 +32,7 @@ end
  #assignments, how can you print out each assignment?
 #You'll need to iterate over your array of room assignments in order to puts out each individual assignment.
 
-def printer(attendees)
+def printer
 
   assign_rooms(attendees).each do |room_assignment|
   #  .push("#{attendees}")
